fb8c8d8a2b435068f9aebd6b5c4ff003c7ffa85b
Yasine31130 <yasine.bns@hotmail.com>
2024-01-19 07:10:23 -0800

    C - Sorting algorithms & Big O

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * quick_sort - to sort with the 'quick sort'
+ * @array: pointer to array
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	(void)*array;
+	(void)size;
+}
